00451e8887dd6c68a1c5b6b2eaa92dbdf167eea0
Manmeet <manmeetkalsi17@gmail.com>
2021-04-28 16:02:21 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ["./src/*.html"],
-  },
+  // purge: {
+  //   enabled: true,
+  //   content: ["./src/*.html"],
+  // },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      serif: ["Playfair Display", "serif"],
+    },
     extend: {
-      fontFamily: {
-        sans: ["Cabin", "sans-serif"],
+      colors: {
+        primary: "#38A3A5",
+        secondary: "#4B4446",
+        accent: "#FFF8EE",
+        dark: "#eae6e0",
       },
     },
   },
